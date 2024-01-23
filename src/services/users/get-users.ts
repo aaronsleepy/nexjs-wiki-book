4ec257fa0ab6039022b662d519aee71f75856f72
Aaron Song <aaron@kmong.com>
2024-01-23 21:21:27 +0900

    feat: getUser API 클라이언트 구현

---
+++ src/services/users/get-users.ts
@@ -0,0 +1,29 @@
+import { ApiContext, User } from "types/data"
+import { fetcher } from "uitls"
+
+export type GetUserParams = {
+    id: number
+}
+  
+/**
+ * 사용자 API(개별 취득)
+ * @param context API 컨텍스트
+ * @param params 파라미터
+ * @returns 사용자
+ */
+const getUser = async (
+    context: ApiContext,
+    { id }: GetUserParams,
+): Promise<User> => {
+    return await fetcher(
+        `${context.apiRootUrl.replace(/\/$/g, '')}/users/${id}`,
+        {
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+        },
+    )
+}
+
+export default getUser
