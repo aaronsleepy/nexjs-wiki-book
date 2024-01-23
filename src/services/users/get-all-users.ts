622c0b0a5bb780d6bfd694e6255252368ce76cdb
Aaron Song <aaron@kmong.com>
2024-01-23 21:22:04 +0900

    feat: getAllUsers API 클라이언트 구현

---
+++ src/services/users/get-all-users.ts
@@ -0,0 +1,19 @@
+import { ApiContext, User } from "types/data"
+import { fetcher } from "uitls"
+
+/**
+ * 사용자 API(목록 취득)
+ * @param context API 컨텍스트
+ * @returns 사용자 목록
+ */
+const getAllUsers = async (context: ApiContext): Promise<User[]> => {
+    return await fetcher(`${context.apiRootUrl.replace(/\/$/g, '')}/users`, {
+      headers: {
+        Origin: '*',
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+  }
+  
+  export default getAllUsers
