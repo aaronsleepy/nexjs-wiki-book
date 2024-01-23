3c6f5d0ec588cd225f533f756b93c6ca85cb2e11
Aaron Song <aaron@kmong.com>
2024-01-23 19:53:16 +0900

    feat: getProduct API 클라이언트 구현

---
+++ src/services/products/get-product.ts
@@ -0,0 +1,30 @@
+import { ApiContext, Product } from "types/data"
+import { fetcher } from "uitls"
+
+export type GetProductParams = {
+    id: number
+  }
+  
+  /**
+   * 제품 API(개별 취득)
+   * @param context API 컨텍스트
+   * @param params 상품 ID
+   * @returns 상품
+   */
+  const getProduct = async (
+    context: ApiContext,
+    { id }: GetProductParams,
+  ): Promise<Product> => {
+    return await fetcher(
+      `${context.apiRootUrl.replace(/\/$/g, '')}/products/${id}`,
+      {
+        headers: {
+          Origin: '*',
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      },
+    )
+  }
+  
+  export default getProduct
