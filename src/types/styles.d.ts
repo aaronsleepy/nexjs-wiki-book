12e5be550b1b55cb0b50d8b51ee92a25c57af9f8
Aaron Song <aaron@kmong.com>
2024-01-24 19:55:46 +0900

    ui: 반응형 디자인(복붙)

--- src/types/styles.d.ts
+++ src/types/styles.d.ts
@@ -2,6 +2,7 @@
  * Responsive 속성
  * - CSS 속성값을 break point별로 설정
  * - T: CSS 속성값의 타입
+ * - Copy & Pasted
  */
 export type ResponsiveProp<T> = {
     base?: T    // 640px 이하
@@ -33,3 +34,105 @@ type ContentDistribution =
  | 'space-between'
  | 'space-evenlty'
  | 'stretch'
+
+ type CSSPropertyGlobals =
+ | '-moz-initial'
+ | 'inherit'
+ | 'initial'
+ | 'revert'
+ | 'unset'
+
+export type CSSPropertyAlignItems =
+ | CSSPropertyGlobals
+ | SelfPosition
+ | 'baseline'
+ | 'normal'
+ | 'stretch'
+ // 코드 자동 보조
+ | (string & {})
+
+export type CSSPropertyAlignContent =
+ | CSSPropertyGlobals
+ | ContentDistribution
+ | 'center'
+ | 'end'
+ | 'flex-end'
+ | 'flex-start'
+ | 'start'
+ | 'baseline'
+ | 'normal'
+ | (string & {})
+
+export type CSSPropertyJustifyItems =
+ | CSSPropertyGlobals
+ | SelfPosition
+ | 'baseline'
+ | 'left'
+ | 'legacy'
+ | 'normal'
+ | 'right'
+ | 'stretch'
+ | (string & {})
+
+export type CSSPropertyJustifyContent =
+ | CSSPropertyGlobals
+ | ContentDistribution
+ | ContentPosition
+ | 'left'
+ | 'normal'
+ | 'right'
+ | (string & {})
+
+export type CSSPropertyFlexWrap =
+ | CSSPropertyGlobals
+ | 'nowrap'
+ | 'wrap'
+ | 'wrap-reverse'
+
+export type CSSPropertyFlexDirection =
+ | CSSPropertyGlobals
+ | 'column'
+ | 'column-reverse'
+ | 'row'
+ | 'row-reverse'
+
+export type CSSPropertyJustifySelf =
+ | CSSPropertyGlobals
+ | SelfPosition
+ | 'auto'
+ | 'baseline'
+ | 'left'
+ | 'normal'
+ | 'right'
+ | 'stretch'
+ | (string & {})
+
+export type CSSPropertyAlignSelf =
+ | CSSPropertyGlobals
+ | SelfPosition
+ | 'auto'
+ | 'baseline'
+ | 'normal'
+ | 'stretch'
+ | (string & {})
+
+/**
+* Grid
+*/
+type GridLine = 'auto' | (string & {})
+
+export type CSSPropertyGridColumn =
+ | CSSPropertyGlobals
+ | GridLine
+ | (string & {})
+
+export type CSSPropertyGridRow = CSSPropertyGlobals | GridLine | (string & {})
+
+export type CSSPropertyGridAutoFlow =
+ | CSSPropertyGlobals
+ | 'column'
+ | 'dense'
+ | 'row'
+ | (string & {})
+
+export type CSSPropertyGridArea = CSSPropertyGlobals | GridLine | (string & {})
